52327c486562f2c38c3d41cd3e206c508e84f434
Zeryllium <zeryllium@gmail.com>
2026-03-12 19:20:30 -0400

    Added session logic to Login and Signup pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,24 +1,37 @@
 import Link from "next/link";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
+
 import CreateLoginForm from "@/components/CreateLoginForm";
+import { auth } from "@/lib/auth";
 
 export default async function Login() {
-  return (
-    <main className="mx-auto max-w-3xl space-y-6 px-4 py-10">
-      <div className="flex items-center justify-between">
-        <h1 className={"text-2xl font-bold"}>Login</h1>
-        <Button asChild variant="ghost">
-          <Link href={"/"}>Back</Link>
-        </Button>
-      </div>
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
 
-      <section className="rounded-lg border bg-card p-6">
-        <CreateLoginForm />
-        <p className={"text-sm"}>
-          {/* TODO: Link colour is not working*/}
-          Don't have an account? <Link href={"/signup"} className={"accent-blue-500 font-bold"}>Register</Link> instead.
-        </p>
-      </section>
-    </main>
-  );
+  if (session) {
+    redirect("/courses")
+  } else {
+    return (
+      <main className="mx-auto max-w-3xl space-y-6 px-4 py-10">
+        <div className="flex items-center justify-between">
+          <h1 className={"text-2xl font-bold"}>Login</h1>
+          <Button asChild variant="ghost">
+            <Link href={"/"}>Back</Link>
+          </Button>
+        </div>
+
+        <section className="rounded-lg border bg-card p-6">
+          <CreateLoginForm />
+          <p className={"text-sm"}>
+            {/* TODO: Link colour is not working*/}
+            Don't have an account? <Link href={"/signup"} className={"accent-blue-500 font-bold"}>Register</Link> instead.
+          </p>
+        </section>
+      </main>
+    );
+  }
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,24 +1,38 @@
 import Link from "next/link";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
+
 import CreateSignUpForm from "@/components/CreateSignUpForm";
+import { auth } from "@/lib/auth";
+
 
 export default async function Login() {
-  return (
-    <main className="mx-auto max-w-3xl space-y-6 px-4 py-10">
-      <div className="flex items-center justify-between">
-        <h1 className={"text-2xl font-bold"}>Sign Up</h1>
-        <Button asChild variant="ghost">
-          <Link href={"/"}>Back</Link>
-        </Button>
-      </div>
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
 
-      <section className="rounded-lg border bg-card p-6">
-        <CreateSignUpForm />
-        <p className={"text-sm"}>
-          {/* TODO: Link colour is not working*/}
-          Have an account? <Link href={"/login"} className={"accent-blue-500 font-bold"}>Login</Link> instead.
-        </p>
-      </section>
-    </main>
-  );
+  if (session) {
+    redirect("/courses")
+  } else {
+    return (
+      <main className="mx-auto max-w-3xl space-y-6 px-4 py-10">
+        <div className="flex items-center justify-between">
+          <h1 className={"text-2xl font-bold"}>Sign Up</h1>
+          <Button asChild variant="ghost">
+            <Link href={"/"}>Back</Link>
+          </Button>
+        </div>
+
+        <section className="rounded-lg border bg-card p-6">
+          <CreateSignUpForm/>
+          <p className={"text-sm"}>
+            {/* TODO: Link colour is not working*/}
+            Have an account? <Link href={"/login"} className={"accent-blue-500 font-bold"}>Login</Link> instead.
+          </p>
+        </section>
+      </main>
+    );
+  }
 }
